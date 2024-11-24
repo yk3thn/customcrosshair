6bb3901db09f4a76a9c290ad33bc4f33b6b609b2
3thn <65327271+yk3thn@users.noreply.github.com>
2024-11-24 13:46:31 -0500

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,6 +1,10 @@
+color 0F
 pip install pygetwindow
 pip show pygetwindow
 pip install pillow
 pip install --upgrade pillow
+
+@echo off
+powershell -Command "Write-Host 'If you see this message and there is no errors above, you can move on.' -ForegroundColor Green"
 
 pause
